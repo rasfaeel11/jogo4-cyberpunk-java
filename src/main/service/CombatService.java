2bbdf96050019ea75b9b5b092d6cb07b97a727ad
Rafael Melo <rafaelmelo01111998@gmail.com>
2025-08-29 19:36:07 -0300

    logica de combate ja foi

--- src/main/service/CombatService.java
+++ src/main/service/CombatService.java
@@ -1,5 +1,43 @@
 package main.service;
+import java.util.Random;
+
+import main.model.Personagem;
 
 public class CombatService {
-    
+    Random random = new Random();
+
+    private static final int EP_GASTO = 5;
+    private static final int VALOR_CURA_STIM = 20;
+
+
+    public int atacarFisico(Personagem Atacante, Personagem Alvo){
+        int d20 = random.nextInt(20) + 1;
+        if(d20 == 1) return 0;
+        int dano = (Atacante.getDanoBase() + Atacante.getForca() + d20 - Alvo.getDefesaFisica());
+        if(dano < 0) dano = 0;
+        if(d20 == 20) dano *= 2;
+        Alvo.receberDanoFisico(dano);
+        return dano;
+    }
+    public int atacarCibernetico(Personagem Atacante, Personagem Alvo){
+        if(Atacante.getEp() < EP_GASTO) return 0;
+        Atacante.gastarEp(EP_GASTO);
+        int d20 = random.nextInt(20) + 1;
+        if(d20 == 1) return 0;
+        int danoMagico = (Atacante.getDanoBase() + Atacante.getInteligencia() + d20 - Alvo.getDefesaCibernetica());
+        if(danoMagico < 0) danoMagico = 0;
+        Alvo.receberDanoCibernetico(danoMagico);
+        return danoMagico;
+    }
+
+    public int usarStim(Personagem personagem, int valorCura){
+        if(personagem.getStims() < 1){
+            return 0;
+        } else {
+            personagem.curar(VALOR_CURA_STIM);
+        }
+        return valorCura;
+        
+
+    }
 }
